22d64ccaa2cf207f357760f91d0360768585c84f
Victor <victorm.martinezjr@outlook.com>
2025-07-03 20:15:45 -0400

    create StarsBackground component

--- frontend/personal-portfolio-frontend/src/App.jsx
+++ frontend/personal-portfolio-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import StarsBackground from "./components/StarsBackground";
 
 function App() {
   return (
-    <>
+    <main className="bg-black w-full h-[100vh] text-white flex justify-center items-center">
+      <StarsBackground />
       <h1>My Portfolio</h1>
-    </>
+    </main>
   );
 }
 
