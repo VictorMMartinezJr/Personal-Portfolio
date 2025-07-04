79bf2f517672d430da3aefaa647ccef6d22cefc3
Victor <victorm.martinezjr@outlook.com>
2025-07-03 20:35:58 -0400

    create Spotlight component

--- frontend/personal-portfolio-frontend/src/App.jsx
+++ frontend/personal-portfolio-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Spotlight from "./components/Spotlight";
 import StarsBackground from "./components/StarsBackground";
 
 function App() {
   return (
     <main className="bg-black w-full h-[100vh] text-white flex justify-center items-center">
       <StarsBackground />
+      <Spotlight />
       <h1>My Portfolio</h1>
     </main>
   );
---
+++ frontend/personal-portfolio-frontend/src/components/Spotlight.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+const Spotlight = () => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full pointer-events-none z-0"
+      >
+        <div className="relative left-1/2 -translate-x-1/2 w-[70%] h-[40vh] bg-gradient-to-b from-white/20 to-transparent blur-3xl" />
+      </motion.div>
+    </>
+  );
+};
+
+export default Spotlight;
