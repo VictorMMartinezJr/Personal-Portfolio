654c13bf05c4e8140e94754fe7abbf164c2157b2
Victor <victorm.martinezjr@outlook.com>
2026-02-24 21:47:34 -0500

    update aboutme text and landing page projects data

--- frontend/personal-portfolio-frontend/src/components/AboutMe.jsx
+++ frontend/personal-portfolio-frontend/src/components/AboutMe.jsx
@@ -11,25 +11,16 @@ const AboutMe = () => {
       <div className="flex flex-col justify-center items-center text-center lg:flex-1">
         <SectionTitle title="About Me" />
         <p className="mt-4 px-4 text-lg sm:text-2xl">
-          Hey there! I'm Victor, a Full Stack Developer building scalable,
-          high-impact applications using Java, Spring Boot, React, and SQL. Most
-          recently, I successfully delivered a legacy indoor navigation system
-          for Gwinnett Technical College, a project years in the making that
-          utilized Dijkstra's algorithm to solve complex campus routing.
-          Following the success of this deployment, I was selected by the
-          Computer Science department as a Founding Member and Team Lead for a
-          new innovation driven student club.
+          Hey there 👋 ! I'm Victor and I specialize in building high-performance applications using React, Java, and Spring Boot.
+          I am the 2026 SkillsUSA Georgia Computer Programming State Champion 🥇and will be representing Georgia in the national championship in June.
         </p>
         <p className="my-4 px-4 text-lg sm:text-2xl">
-          Beyond my 4.0 GPA and representing my college at the SkillsUSA State
-          Programming Competition, I am currently collaborating under a funded
-          NITIC grant to integrate AI driven productivity into full stack
-          workflows and mentor the next generation of developers.
+          Beyond the competition, I earned my Associates Degree in Computer Programming at Gwinnett Technical College with a 4.0 GPA and I am now pursuing my Bachelor's Degree in Computer Science at Kennesaw State University
         </p>
         <p className="mb-4 px-4 text-lg sm:text-2xl">
-          I am seeking a Software Engineering role where I can apply my
-          leadership experience and technical discipline to build the future of
-          software.
+          During my time at Gwinnett Technical College, I Co-Founded and currently serve as President of a new innovation programming club. In this role, I bridge technical
+          craftsmanship with leadership. I am now seeking a Software Engineering/Web Developer role where I can
+          apply this same competitive drive to build the future of software.
         </p>
       </div>
       <div className="flex justify-center items-center lg:flex-1">
--- frontend/personal-portfolio-frontend/src/components/Projects/projectsData.js
+++ frontend/personal-portfolio-frontend/src/components/Projects/projectsData.js
@@ -1,5 +1,7 @@
 import navaid from "../../assets/navaid.png";
 import echosphere from "../../assets/echosphere.png";
+import luxureDetails from "../../assets/luxuredetails.png";
+
 const appsData = [
   {
     name: "EchoSphere",
@@ -37,36 +39,20 @@ const appsData = [
 
 const landingPagesData = [
   {
-    name: "EchoSphere",
+    name: "Luxure Details",
     skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
       "React",
       "TailwindCSS",
-      "Context API",
-      "Java",
-      "Springboot",
-      "MongoDB",
+        "Responsive UI",
     ],
-    imgSrc: echosphere,
+    imgSrc: luxureDetails,
     description:
-        "A full-stack music streaming web app built with React, TailwindCSS, Spring Boot, and MongoDB. Users can browse songs, play music, create accounts, and enjoy a clean, responsive UI. Includes secure JWT authentication, dynamic routing, and real-time audio playback. Features a full Admin Dashboard where authorized users can upload new tracks, edit metadata, and manage the entire music catalog.",
-    GitHub: "https://github.com/VictorMMartinezJr/EchoSphere",
-    demoLink: "https://echosphere-user.netlify.app",
-  },
-  {
-    name: "NavAid",
-    skills: [
-      "React",
-      "React-Konva",
-      "TailwindCSS",
-      "Context API",
-      "Dijkstra's Algorithm",
-      "Framer Motion",
-    ],
-    imgSrc: navaid,
-    description:
-        "An indoor navigation web app that helps students and visitors find the fastest route to any room in Building A at Gwinnett Tech. Built an interactive map canvas, real-time Dijkstra pathfinding, search, and dynamic coordinate scaling for accurate navigation. Developed in collaboration with the Computer Science department at my college, and the project led to being invited as a co-founder of a new student innovation/programming club.",
-    GitHub: "https://github.com/VictorMMartinezJr/Personal-Portfolio",
-    demoLink: "https://navaid-v1.netlify.app",
+        "A landing page for a luxury car detailing brand. Fully responsive for all devices and optimized images.",
+    GitHub: "",
+    demoLink: "https://luxuredetails-v1.netlify.app",
   },
 ];
 
